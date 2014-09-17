8803bf2005bf188947468870cd6bdd89ffffc6de
my <my@qq.com>
2014-09-17 11:27:43 +0800

    s

--- core/Zlib/Conf/db.php
+++ core/Zlib/Conf/db.php
@@ -9,11 +9,11 @@
 return array(
 
 	'DB_TYPE'               =>  'mysql',     	// 数据库类型
-    'DB_HOST'               =>  'localhost', 	// 服务器数据库地址
-    'DB_NAME'               =>  'zlang',     		// 数据库名
-    'DB_USER'               =>  'root',     	// 用户名
-    'DB_PWD'                =>  '',         	// 密码
-    'DB_PORT'               =>  '3306',        	// 端口
+    'DB_HOST'               =>  '172.16.6.200', 	// 服务器数据库地址
+    'DB_NAME'               =>  'zl',     		// 数据库名
+    'DB_USER'               =>  'zhulang',     	// 用户名
+    'DB_PWD'                =>  'zhulang',      // 密码
+    'DB_PORT'               =>  '3307',        	// 端口
     'DB_PREFIX'             =>  '',    		// 数据库表前缀
     'DB_FIELDTYPE_CHECK'    =>  false,      // 是否进行字段类型检查
     'DB_FIELDS_CACHE'       =>  true,       // 启用字段缓存
