1354f768c93899cd2641fdcb0025225bdd5577d8
songmw <songmw@qq.com>
2014-11-13 06:13:20 +0800

    home

---
+++ Application/Admin/Controller/TypeController.class.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * 职位分类
+ *
+ * @author wangz
+ * @date   2014-11-11
+ */
+
+namespace Admin\Controller;
+use Common\Service\Category;
+
+class TypeController extends BaseController {
+
+	protected $typeInstance = null;
+	protected $typeModel = null;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->typeInstance = Category::getInstance();
+		$this->typeModel = M('JobCategory');
+	}
+
+	public function index()
+	{
+		$this->assign('category', $this->typeInstance->mCategory);
+		$this->display();
+	}
+
+	public function edit()
+	{
+		$id = I('get.id');
+
+		// 获取分类信息
+		$info = $this->typeModel->where('id='.$id)->find();
+		$cate_class = $this->typeInstance->getClass();
+
+		$this->assign('cate_class', $cate_class);
+		$this->assign('info', $info);
+		$this->display();
+	}
+
+	public function doEdit()
+	{
+		if (IS_POST) {
+			$data = I();
+			
+			if ($data['pid'] == 0) {
+				$data['path'] = 0;
+			} else {
+				$parent_info = $this->typeModel->where('id='.$data['pid'])->find();
+				$data['path'] = $parent_info['path'].'_'.$data['pid'];
+			}
+
+			$rs = $this->typeModel->data($data)->save();
+			$this->typeInstance->clear();
+
+			$msg['code'] = 1;
+			$msg['msg'] = '修改成功';
+			return $this->ajaxReturn($msg);
+		}
+	}
+
+}
